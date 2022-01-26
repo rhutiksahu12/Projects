ba02bb895525a14fd450214820bdc9a054f88441
Rhutik Sahu <shubhamsahu938@gmail.com>
2022-01-26 04:42:40 +0100

    new project started

---
+++ src/components/form.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function from() {
+  return <div>
+      <div>
+      <form>
+        <div>
+          <label>Check Number:</label>
+          <input type="text" />
+          <label> Amount: </label>
+          <input type="number" />
+        </div>
+      </form>
+    </div> 
+  </div>;
+}
+
+export default from;
